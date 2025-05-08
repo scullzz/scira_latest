c36f1929985e0f567b3238583a5bdc2b7723834e
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-05-08 22:06:56 +0500

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-// /lib/utils.ts
+// /lib/utils.ts bro
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { Globe, Book, YoutubeIcon, TelescopeIcon } from 'lucide-react'
